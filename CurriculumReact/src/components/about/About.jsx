7196e282ccda7e5f4233bbe89141eaf39b986ac1
Garridok <mrgarrido98@gmail.com>
2022-07-14 17:21:04 +0200

    Start Experience

--- CurriculumReact/src/components/about/About.jsx
+++ CurriculumReact/src/components/about/About.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
-import './about.css'
-
+import React from "react";
+import "./about.css";
+import ME from "../../asset/me-about.jpg";
+import {RiAwardFill} from 'react-icons/ri'
+import {FiUsers} from 'react-icons/fi'
+import {BiFolderOpen} from 'react-icons/bi'
 
 const About = () => {
   return (
-    <section id="about" >About</section>
-  )
-}
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
 
-export default About
+      <div className="container about_container">
+        <div className="about_me">
+          <img className="about_me-image" src={ME} alt="me" />
+        </div>
+
+        <div className="about_content">
+          <div className="about_cards">
+          <article className="about_card">
+                <RiAwardFill className="about_icon" />
+              <h5>Experience</h5>
+              <small>2+ Years in coding</small>
+            </article>
+
+            <article className="about_card">
+                <FiUsers className="about_icon" />
+              <h5>Clients</h5>
+              <small>2+ Clients</small>
+            </article>
+
+            <article className="about_card">
+                <BiFolderOpen className="about_icon" />
+              <h5>Proyects</h5>
+              <small>10+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem porro saepe necessitatibus libero quod aperiam, quia dolor cumque voluptate laborum odio temporibus sint voluptates quisquam quo soluta consequatur minima expedita!
+          </p>
+
+          <a href="#contact" className="btn btn-primary">Let's Talk</a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- CurriculumReact/src/components/experience/Experience.jsx
+++ CurriculumReact/src/components/experience/Experience.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import './experience.css'
+import React from "react";
+import "./experience.css";
+import {HiBadgeCheck} from 'react-icons/hi'
 
 
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
-  )
-}
+    <section id="experience">
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
 
-export default Experience
+      <div className="container experience_container">
+        <div className="experience_frontend">
+            <h3>FrontEnd Development</h3>
+            <div className="experience_content">
+              <article></article>
+            </div>
+        </div>
+
+        <div className="experience_backend">
+
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Experience;
