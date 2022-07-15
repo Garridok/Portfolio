cbcd58df62ebd2fe77500e19048547a78e77bcdd
Garridok <mrgarrido98@gmail.com>
2022-07-15 22:59:06 +0200

    portfolio

--- CurriculumReact/src/components/portfolio/Portfolio.jsx
+++ CurriculumReact/src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,39 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../asset/portfolio1.jpg'
+import IMG2 from '../../asset/portfolio2.jpg'
+import IMG3 from '../../asset/portfolio3.jpg'
+import IMG4 from '../../asset/portfolio4.jpg'
+
+
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Crypto',
+    github: '#',
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Crypto',
+    github: '#',
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Crypto',
+    github: '#',
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Crypto',
+    github: '#',
+  },
+]
+
 
 
 const Portfolio = () => {
@@ -8,13 +42,23 @@ const Portfolio = () => {
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
 
-      <div className="container portfolio_container">
-        <article className='portfolio_item'>
-            <div className="portfolio_item-image"></div>
-            <h3>This is a Portfolio item title</h3>
-            <a href="#" className='btn' target='_blank'>Github</a>
+      <div className="container portfolio_container"> 
+       {
+          data.map(({id, image, title, github}) => {
+            return (
+              <article key={id} className='portfolio_item'>
+            <div className="portfolio_item-image">
+              <img src={image} alt="portfolio" />
+            </div>
+            <h3>{title}</h3>
+            <div className="portfolio_item-cta">
+              <a href={github} className='btn' target='_blank'>Github</a>
+            </div>
         </article>
-
+            )
+          }
+          )
+        }
 
 
       </div>
