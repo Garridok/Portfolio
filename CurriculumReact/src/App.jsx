fd94dfa23c779d1d4d0293c933e4babada07fcec
Garridok <mrgarrido98@gmail.com>
2022-07-16 23:01:22 +0200

    version Lorem

--- CurriculumReact/src/App.jsx
+++ CurriculumReact/src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 import './App.css'
 import About from './components/about/About'
-import Contact from './components/contact/contact'
+import Contact from './components/contact/Contact'
 import Experience from './components/experience/Experience'
+import Footer from './components/footer/Footer'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import Portfolio from './components/portfolio/Portfolio'
@@ -20,6 +21,7 @@ function App() {
         <Services />
         <Portfolio />
         <Contact />
+        <Footer />
     </>
  )
 }
--- CurriculumReact/src/components/footer/Footer.jsx
+++ CurriculumReact/src/components/footer/Footer.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import './footer.css'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
+
 
 const Footer = () => {
   return (
-   <div>footer</div>
+   <footer>
+      <a href="#" className='footer_logo'>GarridoK</a>
+
+      <ul className='permalinks'>
+          <li><a href="#">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#experience">Experience</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#portfolio">Porfolio</a></li>
+          <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer_socials">
+      <a href="https://www.linkedin.com/in/antonio-garrido-carranza-2a021b210/" target="_blank"><BsLinkedin  /></a>
+        <a href="https://github.com/Garridok" target="_blank"><BsGithub /></a>
+      </div>
+
+      <div className="footer_copyright">
+        <small>&copy; GarridoK</small>
+      </div>
+   </footer>
   )
 }
 
